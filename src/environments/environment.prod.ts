5a4c296f3d95908695b09e9ad6d5615804f28bfd
Андрей Ашмянец <andreka6033@gmail.com>
2022-11-30 17:09:55 +0300

    best commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://social-network.samuraijs.com/api/1.1',
+  baseUrl: 'http://localhost:8080',
   apiKey: '76ca6c52-f24e-42fc-86f5-361b406922b4',
 }
+//https://social-network.samuraijs.com/api/1.1
