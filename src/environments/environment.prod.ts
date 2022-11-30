842e4894b8c0736de67225613464ad41e54eb43d
Андрей Ашмянец <andreka6033@gmail.com>
2022-11-30 15:21:23 +0300

    pg pages commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  baseUrl: 'https://social-network.samuraijs.com/api/1.1',
+  apiKey: '76ca6c52-f24e-42fc-86f5-361b406922b4',
+}
